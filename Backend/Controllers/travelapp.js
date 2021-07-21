0839099e22f50fd61df2533e3be2c3942db519d6
gopi <gopi15699@gmail.com>
2021-07-21 15:20:25 +0530

    committed

---
+++ Backend/Controllers/travelapp.js
@@ -0,0 +1,9 @@
+const City = require('../Models/city');
+const Hotel = require('../Models/hotel');
+const Restaurant = require('../Models/restaurant');
+
+
+
+exports.addCity = (req,res)=>{
+
+}
---
+++ Backend/Routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+const { addCity } = require('../Controllers/travelapp');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
